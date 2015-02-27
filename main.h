e4d10fc3e1d4cfa0805f9b11d239b6df5e9600da
Arild M Johannessen <arildj78@gmail.com>
2015-02-27 18:48:41 +0100

    Course() renamed CourseInitial() Declaration for IntermediatePoint() added

--- main.h
+++ main.h
@@ -13,7 +13,8 @@
 #define D2R 0.01745329251994329576923690768489 //multiply degrees with D2R to get radians
 
 double _stdcall Distance(const double* lat1, const double* lon1, const double* lat2, const double* lon2);
-double _stdcall Course (double *lat1, double *lon1, double *lat2, double *lon2);
+double _stdcall CourseInitial (double *lat1, double *lon1, double *lat2, double *lon2);
+void _stdcall IntermediatePoint (const double *lat1, const double *lon1, const double *lat2, const double *lon2, const double *fraction, double *latresult, double *lonresult)
 
 long long SpeedTest();
 int main();
