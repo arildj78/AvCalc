8a1ea854f7d90dc9174bfab572d5f9aabc212e7e
Arild M Johannessen <arildj78@gmail.com>
2025-12-10 18:32:35 +0100

    Moved Standard_temperature to of the new section on ICAO standard atmosphere. Also create a new section on top pertaining to navigation.

--- tests/test_AvCalc.c
+++ tests/test_AvCalc.c
@@ -10,6 +10,42 @@ void tearDown(void) {
     // Run after each test
 }
 
+void test_Distance_LAX_to_JFK(void) {
+    double lat1 = 33.95;
+    double lon1 = -118.4;
+    double lat2 = 40.633333;
+    double lon2 = -73.783333;
+    
+    double dist = Distance(&lat1, &lon1, &lat2, &lon2);
+    
+    TEST_ASSERT_FLOAT_WITHIN(5.0, 2144.0, dist);
+}
+
+void test_CourseInitial_LAX_to_JFK(void) {
+    double lat1 = 33.95;
+    double lon1 = -118.4;
+    double lat2 = 40.633333;
+    double lon2 = -73.783333;
+    
+    double course = CourseInitial(&lat1, &lon1, &lat2, &lon2);
+    
+    TEST_ASSERT_FLOAT_WITHIN(0.5, 66.0, course);
+}
+
+void test_IntermediatePoint(void) {
+    double lat1 = 33.95;
+    double lon1 = -118.4;
+    double lat2 = 40.633333;
+    double lon2 = -73.783333;
+    double fraction = 0.4;
+    double lat_result, lon_result;
+    
+    IntermediatePoint(&lat1, &lon1, &lat2, &lon2, &fraction, &lat_result, &lon_result);
+    
+    TEST_ASSERT_FLOAT_WITHIN(0.02, 38.66945, lat_result);
+    TEST_ASSERT_FLOAT_WITHIN(0.05, -101.62617, lon_result);
+}
+
 void test_Standard_temperature(void) {
     // Standard atmosphere temperatures at various altitudes
     // Format: altitude in feet, expected temperature in Celsius
@@ -58,42 +94,6 @@ void test_Standard_temperature(void) {
     }
 }
 
-void test_Distance_LAX_to_JFK(void) {
-    double lat1 = 33.95;
-    double lon1 = -118.4;
-    double lat2 = 40.633333;
-    double lon2 = -73.783333;
-    
-    double dist = Distance(&lat1, &lon1, &lat2, &lon2);
-    
-    TEST_ASSERT_FLOAT_WITHIN(5.0, 2144.0, dist);
-}
-
-void test_CourseInitial_LAX_to_JFK(void) {
-    double lat1 = 33.95;
-    double lon1 = -118.4;
-    double lat2 = 40.633333;
-    double lon2 = -73.783333;
-    
-    double course = CourseInitial(&lat1, &lon1, &lat2, &lon2);
-    
-    TEST_ASSERT_FLOAT_WITHIN(0.5, 66.0, course);
-}
-
-void test_IntermediatePoint(void) {
-    double lat1 = 33.95;
-    double lon1 = -118.4;
-    double lat2 = 40.633333;
-    double lon2 = -73.783333;
-    double fraction = 0.4;
-    double lat_result, lon_result;
-    
-    IntermediatePoint(&lat1, &lon1, &lat2, &lon2, &fraction, &lat_result, &lon_result);
-    
-    TEST_ASSERT_FLOAT_WITHIN(0.02, 38.66945, lat_result);
-    TEST_ASSERT_FLOAT_WITHIN(0.05, -101.62617, lon_result);
-}
-
 void test_Speed_of_sound(void) {
     double oat = -6.72;
     double cs = Speed_of_sound(&oat);
@@ -129,8 +129,9 @@ int main(void) {
     RUN_TEST(test_Distance_LAX_to_JFK);
     RUN_TEST(test_CourseInitial_LAX_to_JFK);
     RUN_TEST(test_IntermediatePoint);
-    RUN_TEST(test_Speed_of_sound);
+
     RUN_TEST(test_Standard_temperature);
+    RUN_TEST(test_Speed_of_sound);
     RUN_TEST(test_Density_at_sea_level);
     RUN_TEST(test_Pressure_at_sea_level);
     RUN_TEST(test_Pressure_at_tropopause);
